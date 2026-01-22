98dd68352695194008a0df44394b80112b51c3c9
Jennifer Petrus <jenny.petrus19@gmail.com>
2026-01-22 19:03:26 +0100

    [started] to learn python

---
+++ Day1/BandNameGenerator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+
+city = input("Which city did you grow up in?\n")
+pet = input("What is your favourite pets name?\n")
+
+print("Your band name is " + city + " " + pet)
