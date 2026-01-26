bf34343808ee831bb54e672c476245e3dc5a72c5
Jennifer Petrus <jenny.petrus19@gmail.com>
2026-01-26 20:49:31 +0100

    [Day 6] Escape the Maze

---
+++ Day 6/EscapeTheMaze.py
@@ -0,0 +1,47 @@
+# Escape the Maze: Reeborg's World Maze
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# while front_is_clear():
+#     move()
+# turn_left()
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+
+# Reeborg's Wolrd Hurdle 4:
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+    
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+# while loop
+# while something_is_true:
+#   do this
+#   then do this
+#   then do this
