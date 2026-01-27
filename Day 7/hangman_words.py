2ce1a3ade1406f00db803e9b34ae01adb0141d3e
Jennifer Petrus <jenny.petrus19@gmail.com>
2026-01-27 21:19:11 +0100

    [Day 7] Hangman

---
+++ Day 7/hangman_words.py
@@ -0,0 +1,8 @@
+word_list = [
+    "mouse", "baboon", "camel", "elephant", "giraffe", 
+    "kangaroo", "leopard", "penguin", "turtle", "dolphin",
+    "octopus", "hamster", "squirrel", "raccoon", "vulture",
+    "badger", "cheetah", "lizard", "hyena", "iguana",
+    "jacket", "puzzle", "whiskey", "matrix", "oxygen",
+    "galaxy", "zigzag", "wizard", "quartz", "rhubarb"
+]
